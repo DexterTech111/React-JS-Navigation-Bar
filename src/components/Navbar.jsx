e7b08cd062eb687424ea6f2af77fc247802d630f
DexterTech <62775645+DexterTech111@users.noreply.github.com>
2023-10-06 16:50:35 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     }
     return (
         <div className='text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>infoDexter.</h1>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Info-Dexter.</h1>
             <ul className=' hidden md:flex'>
                 
                 <li className='p-4'>Name</li>
